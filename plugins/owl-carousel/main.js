9e2affda6641b9629766b5730feec45316f1c420
Brij Rathod <br1jrathod211@gmail.com>
2025-12-31 02:48:47 +0530

    Plugins

---
+++ plugins/owl-carousel/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 4,
+    loop: true,
+    margin: 15,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    nav: true,
+    dots: false
+  });
+});
